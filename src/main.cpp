343d7a3b1884043cf27cf2defe1894f742cfe915
submarines-and <cached_lifters2j@icloud.com>
2026-01-02 16:59:58 +0100

    fixed code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
 #ifdef ESP8266
-#define MOTION_INPUT_PIN D5
+#define MOTION_INPUT_PIN A0
 #define LIGHT_OUTPUT_PIN D6
 #else
 #define MOTION_INPUT_PIN PB0
@@ -10,17 +10,22 @@
 
 void setup()
 {
-    pinMode(MOTION_INPUT_PIN, INPUT);
+    Serial.begin(115200);
     pinMode(LIGHT_OUTPUT_PIN, OUTPUT);
-    digitalWrite(LIGHT_OUTPUT_PIN, LOW);
 }
 
 void loop()
 {
-    int isOn = digitalRead(MOTION_INPUT_PIN);
-    if (isOn == HIGH) {
+    int lightValue = analogRead(MOTION_INPUT_PIN);
+
+    // log in debug
+#ifdef ESP8266
+    Serial.println(lightValue);
+#endif
+
+    if (lightValue > 25 && lightValue < 500) {
         digitalWrite(LIGHT_OUTPUT_PIN, HIGH);
-        //  delay(5000);
+        delay(20 * 1000); // on duration
     }
     else {
         digitalWrite(LIGHT_OUTPUT_PIN, LOW);
